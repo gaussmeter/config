14d94f4ef94a04021827d791b6ffdfbbd6452abb
Leland Sindt <leland@sway.org>
2018-12-18 21:12:12 -0600

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM golang AS builder
+COPY ./src/config.go ./config.go
+RUN mkdir -p /go/src/github.com/docker/ && cd /go/src/github.com/docker && git clone --depth 1 https://github.com/docker/docker
+#RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main . 
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -a -installsuffix cgo -o config . 
+FROM alpine AS main 
+COPY --from=builder /go/config ./config 
+COPY ./src/ ./ 
+CMD ["./config"]
---
+++ src/config.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+  "context"
+  "os"
+  "github.com/docker/docker/api/types"
+  "github.com/docker/docker/api/types/swarm"
+  "html/template"
+  "github.com/docker/docker/client"
+  "log"
+  "net/http"
+)
+
+
+type gauss struct {
+  GaussUserName string
+  GaussPassword string
+}
+
+func createSecret(secretString string, secretName string) (string) {
+  cli, err :=  client.NewClientWithOpts(client.WithVersion("1.38"))
+  if err != nil {panic(err)}
+  secretannotation := swarm.Annotations{Name:secretName, Labels:nil}
+  secretdata :=[]byte(secretString)
+  response, err := cli.SecretCreate(context.Background(), swarm.SecretSpec{
+    secretannotation, secretdata, nil,nil,
+  })
+  if err != nil {
+    log.Print(err)
+    return ""
+  }
+  return response.ID
+}
+
+func createService(serviceName string, imageName string) (string) {
+  cli, err := client.NewClientWithOpts(client.WithVersion("1.38"))
+  if err != nil {panic(err)}
+
+  var serviceSpec swarm.ServiceSpec
+  containerSpec := &swarm.ContainerSpec{Image: imageName}
+  serviceSpec.TaskTemplate.ContainerSpec = containerSpec
+  annotations := swarm.Annotations{Name:serviceName}
+  serviceSpec.Annotations = annotations
+  fileMode := os.FileMode(uint32(0))
+  secretReferenceFileTarget := &swarm.SecretReferenceFileTarget{"/secret/gaussPassword","0","0",fileMode}
+  secret := &swarm.SecretReference{File:secretReferenceFileTarget,SecretID:getSecretID("GaussPassword"),SecretName:"GaussPassword"}
+  serviceSpec.TaskTemplate.ContainerSpec.Secrets = append(serviceSpec.TaskTemplate.ContainerSpec.Secrets, secret)
+
+  secretReferenceFileTarget = &swarm.SecretReferenceFileTarget{"/secret/gaussUserName","0","0",fileMode}
+  secret = &swarm.SecretReference{File:secretReferenceFileTarget,SecretID:getSecretID("GaussUserName"),SecretName:"GaussUserName"}
+  serviceSpec.TaskTemplate.ContainerSpec.Secrets = append(serviceSpec.TaskTemplate.ContainerSpec.Secrets, secret)
+
+  var serviceCreateOptions types.ServiceCreateOptions
+  response, err := cli.ServiceCreate(context.Background(),serviceSpec, serviceCreateOptions)
+  if err != nil {
+    log.Print(err)
+    return ""
+  }
+  return response.ID
+}
+
+func deleteSecret(secretName string) {
+  cli, err := client.NewClientWithOpts(client.WithVersion("1.38"))
+  if err != nil { panic(err)}
+  err = cli.SecretRemove(context.Background(), getSecretID(secretName) )
+  if err != nil {
+    log.Print(err)
+  }
+}
+
+func deleteService(serviceName string){
+  cli, err := client.NewClientWithOpts(client.WithVersion("1.38"))
+  if err != nil { panic(err)}
+  err = cli.ServiceRemove(context.Background(), getServiceID(serviceName))
+  if err != nil {
+    log.Print(err)
+  }
+}
+
+func getSecretID(secretName string) (string) {
+  cli, err := client.NewClientWithOpts(client.WithVersion("1.38"))
+  if err != nil { panic(err)}
+  secrets, err := cli.SecretList(context.Background(),types.SecretListOptions{})
+  if err != nil {
+    log.Print(err)
+    return ""
+  }
+  for _, secret := range secrets {
+    if secret.Spec.Name == secretName {
+      return secret.ID
+    }
+  }
+  return ""
+}
+
+func getServiceID(secretName string) (string) {
+  cli, err := client.NewClientWithOpts(client.WithVersion("1.38"))
+  if err != nil { panic(err)}
+  services, err := cli.ServiceList(context.Background(), types.ServiceListOptions{})
+  if err != nil {
+    log.Print(err)
+    return ""
+  }
+  for _, service := range services {
+    if service.Spec.Name == secretName {
+      return service.ID
+    }
+  }
+  return ""
+}
+
+
+func gaussHandler(w http.ResponseWriter, r *http.Request) {
+  if r.FormValue("submit") == "submit" {
+    log.Print("submit")
+    deleteService("query")
+    deleteSecret("GaussUserName")
+    deleteSecret("GaussPassword")
+    createSecret(r.FormValue("GaussUserName"),"GaussUserName")
+    createSecret(r.FormValue("GaussPassword"),"GaussPassword")
+    createService("query","gaussmeter/query")
+  }
+  f := gauss{r.FormValue("GaussUserName"), r.FormValue("GaussPassword")}
+  t, err := template.ParseFiles("gauss.html")
+  if err != nil {
+    log.Print(err)
+  }
+  err = t.Execute(w, f)
+  if err != nil {
+    log.Print(err)
+  }
+}
+
+func main() {
+  http.HandleFunc("/gauss", gaussHandler)
+  log.Fatal(http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil))
+}
