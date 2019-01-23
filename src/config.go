68c3ffb28880de1c1c8cb07a204330fed63f217c
Leland Sindt <leland@sway.org>
2019-01-23 07:02:16 -0600

    todo

--- src/config.go
+++ src/config.go
@@ -138,6 +138,7 @@ func badgerPut(w http.ResponseWriter, r *http.Request) {
 	log.Printf("badger PUT Key: %s Value: %s \n",key,data)
 }
 
+// Todo: get the stream from badger, then marshal, then return to client.
 func badgerStream(w http.ResponseWriter, r *http.Request){
 	prefix := mux.Vars(r)["prefix"]
 	log.Printf("streamr GET prefix: %s", prefix)
